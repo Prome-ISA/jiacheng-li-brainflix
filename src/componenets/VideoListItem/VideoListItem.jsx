7ab5a3a4bd5fa0f810356d0b2fcd76dee4978af6
Prome-ISA <l1109699036@gmail.com>
2024-03-03 15:46:34 -0500

    finished setpu api to display videos on the home page

--- src/componenets/VideoListItem/VideoListItem.jsx
+++ src/componenets/VideoListItem/VideoListItem.jsx
@@ -1,9 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./VideoListItem.scss"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL= "https://unit-3-project-api-0a5620414506.herokuapp.com/videos";
+
+const API_KEY ="?api_key=f9e031c2-65ee-405b-a5a7-16337ca7063b"
 
 function VideoListItem({ video, isActive, updateActiveVideoInfo }) {
 
     const { title, id, image, channel } = video;
+    // const {id, title, image, channel} =useParams();
+    // const [videoData, setVideoData] = useState(null);
+
+    // useEffect(() =>{
+    //     axios.get(`${API_URL}/${id}${API_KEY}`)
+    //   .then(response => {
+    //     setVideoData(response.data);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching data:', error);
+    //   });
+    // },[id])
 
 
 
@@ -23,10 +41,10 @@ function VideoListItem({ video, isActive, updateActiveVideoInfo }) {
         <li onClick={handleClick} className={className}>
             <Link to={`/videos/${id}`}> 
             
-            <img className="videoListItem__img" src={image} alt={title} />
+            <img className="videoListItem__img" src={video.image} alt={video.title} />
             <div className="videoListItem__titleAndChannel">
-                <p className="videoListItem__titleAndChannel--title">{title}</p>
-                <p className="videoListItem__titleAndChannel--channel">{channel}</p>
+                <p className="videoListItem__titleAndChannel--title">{video.title}</p>
+                <p className="videoListItem__titleAndChannel--channel">{video.channel}</p>
             </div>
             </Link>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import axios from 'axios';
 
-import videos from "../../data/videos.json"
+// import videos from "../../data/videos.json"
 import videoInfo from "../../data/video-details.json";
 
 import Navbar from "../../componenets/NavBar/NavBar.jsx";
@@ -10,35 +11,64 @@ import VideoDetails from "../../componenets/VideoDetails/VideoDetails.jsx";
 import CommentSection from "../../componenets/CommentSection/CommentSection.jsx";
 import VideoList from "../../componenets/VideoList/VideoList.jsx";
 
+const API_URL= "https://unit-3-project-api-0a5620414506.herokuapp.com/videos";
+
+const API_KEY ="?api_key=f9e031c2-65ee-405b-a5a7-16337ca7063b"
 
 
+function HomePage() {
 
+  const [activeVideoInfo, setActiveVideoInfo] = useState(videoInfo[0]);
+  const [videos, setVideos] = useState([]);
+  const {id} = useParams();
 
-function HomePage(){
-    const [activeVideoInfo, setActiveVideoInfo] = useState(videoInfo[0]);
-  {/*const [activeVideo, setActiveVideo] = useState(videos[0]);*/ }
-
-
-  function updateActiveVideoInfo(clickVideoID) {
-    {/*const newActiveVideo = videos.find((video) => video.id === clickVideoID)*/ }
-    const newActiveVideoInfo = videoInfo.find((videoInfo) => videoInfo.id === clickVideoID)
-
-    {/*setActiveVideo(newActiveVideo)*/ }
-
-    setActiveVideoInfo(newActiveVideoInfo)
+  const fetchVideos = async ()=>{
+      try {
+        const response = await axios.get(`${API_URL}/${API_KEY}`);
+      console.log(response.data)//test code
+      setVideos(response.data)
+      
+        
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      
   }
 
-  return(
-    <main className="app">
-      
-      <VideoPlayer videoInfo={activeVideoInfo}/> 
-      <VideoDetails videoInfo={activeVideoInfo}  /> 
-      <CommentSection videoInfo={activeVideoInfo}/> 
-      {/**/} 
+  useEffect( () => {
+      fetchVideos();
+  },[id])
+  
+  // function updateActiveVideoInfo(clickVideoID) {
 
-      <VideoList 
-        videos={videos} 
-        activeVideoInfo={activeVideoInfo} 
+  //   const newActiveVideoInfo = videoInfo.find((videoInfo) => videoInfo.id === clickVideoID)
+
+
+  //   setActiveVideoInfo(newActiveVideoInfo)
+  // }
+
+  function updateActiveVideoInfo(clickVideoID) {
+    // Fetch the clicked video info from the API and update the state
+    axios.get(`${API_URL}/${clickVideoID}${API_KEY}`)
+      .then(response => {
+        setActiveVideoInfo(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  return (
+    <main className="app">
+
+      <VideoPlayer videoInfo={activeVideoInfo} />
+      <VideoDetails videoInfo={activeVideoInfo} />
+      <CommentSection videoInfo={activeVideoInfo} />
+      {/**/}
+
+      <VideoList
+        videos={videos}
+        activeVideoInfo={activeVideoInfo}
         updateActiveVideoInfo={updateActiveVideoInfo}
       />
 
